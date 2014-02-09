3fa3eb126520853f0ce0e966ea187b4cb7782557
Alejandro Rupérez Hernando <alexruperez@gmail.com>
2014-02-09 20:42:30 +0100

    Initial Commit

---
+++ MultipeerConnectivityExampleTests/MultipeerConnectivityExampleTests.m
@@ -0,0 +1,34 @@
+//
+//  MultipeerConnectivityExampleTests.m
+//  MultipeerConnectivityExampleTests
+//
+//  Created by alexruperez on 09/02/14.
+//  Copyright (c) 2014 alexruperez. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MultipeerConnectivityExampleTests : XCTestCase
+
+@end
+
+@implementation MultipeerConnectivityExampleTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
